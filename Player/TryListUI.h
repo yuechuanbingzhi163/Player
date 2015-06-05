61360d19d98beccba385cc1aae84253f04cbf126
yasin2010 <709699023@qq.com>
2015-06-05 18:42:07 +0800

    优化界面

--- Player/TryListUI.h
+++ Player/TryListUI.h
@@ -2,15 +2,28 @@
 #define _TRY_LISTUI_H
 
 
+static double CalculateDelay(double state)
+{
+	return pow(state, 2);
+}
+
 class CTryListUI : public CListUI
 {
+	enum 
+	{
+		SCROLL_TIMERID = 11,
+	};
 public:
 	CTryListUI();
 	~CTryListUI();
 	BOOL	AddListNode(int musicId,LPCTSTR lpName,LPCTSTR lpSinger, LPCTSTR lpAlbtm, LPCTSTR lpTimer);
 	bool	SelectItem(int index,bool bTakeFocus = false);
+	void	DoEvent(TEventUI& event);
 private:
 	CDialogBuilder  m_dlgItemBuilder;
+	int		delay_deltaY_;
+	int		delay_number_;
+	int		delay_left_;
 };
 
 #endif
